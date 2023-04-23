451e3b731ac07a99e5c5264a5e05af36bd272031
Adrian07 <adrian.mirabal07@gmail.com>
2023-04-23 12:50:34 -0400

    Add project files.

---
+++ CurstomersApi/Controllers/CustomerController.cs
@@ -0,0 +1,52 @@
+﻿using CurstomersApi.Dtos;
+using CurstomersApi.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CurstomersApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CustomerController : Controller
+    {
+        private readonly CustomerDatabaseContext customerDatabaseContext;
+        public CustomerController(CustomerDatabaseContext customerDatabaseContext)
+        {
+            this.customerDatabaseContext = customerDatabaseContext;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(CustomerDto))]
+        public async Task<List<CustomerDto>> GetCustomer() => throw new NotImplementedException();
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(CustomerDto))]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetCustomer(long id)
+        {
+            CustomerEntity result = await customerDatabaseContext.Get(id);
+            return new OkObjectResult(result.ToDto());
+        }
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(CustomerDto))]
+        public async Task<bool> DeleteCustomer(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created, Type = typeof(CustomerDto))]
+        public async Task<IActionResult> CreateCustomer(CreateCustomerDto customer)
+        {
+            CustomerEntity result = await customerDatabaseContext.Add(customer);
+            return new CreatedResult($"http://localhost:7030/api/customer/{result.Id}", null);
+        }
+
+        [HttpPut]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(CustomerDto))]
+        public async Task<CustomerDto> UpdateCustomer(CustomerDto customer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CurstomersApi/Repositories/CustomerDatabaseContext.cs
@@ -0,0 +1,58 @@
+﻿using CurstomersApi.Dtos;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace CurstomersApi.Repositories
+{
+    public class CustomerDatabaseContext : DbContext
+    {
+        public CustomerDatabaseContext(DbContextOptions<CustomerDatabaseContext> options) : base(options)
+        {
+
+        }
+        public DbSet<CustomerEntity> Customer { get; set; }
+        public async Task<CustomerEntity> Get(long id)
+        {
+            return await Customer.FirstAsync(x => x.Id == id);
+        }
+
+        public async Task<CustomerEntity> Add(CreateCustomerDto customerDto)
+        {
+            CustomerEntity entity = new CustomerEntity()
+            {
+                Id = null,
+                Address = customerDto.Address,
+                Email = customerDto.Email,
+                FirstName = customerDto.FirstName,
+                LastName = customerDto.LastName,
+                Phone = customerDto.Phone,
+            };
+            EntityEntry<CustomerEntity> response = await Customer.AddAsync(entity);
+            await SaveChangesAsync();
+            return await Get(response.Entity.Id ?? throw new Exception("No se ha podido conectar"));
+        }
+    }
+
+    public class CustomerEntity
+    {
+        public long? Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+
+        public CustomerDto ToDto()
+        {
+            return new CustomerDto()
+            {
+                Address = Address,
+                Email = Email,
+                FirstName = FirstName,
+                LastName = LastName,
+                Phone = Phone,
+                Id = Id ?? throw new Exception("El id no puede ser null")
+            };
+        }
+    }
+}
